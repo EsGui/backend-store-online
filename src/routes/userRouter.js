4c39bc41a87bd97f98590574d7804370bbb652b0
EsGui <guilhermedagama2021100@gmail.com>
2022-10-11 13:15:07 -0300

    feat: Controller de usuário foi adicionado na rota de usuário

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const userController = require('../controllers/usersController');
 
 const { Router } = express;
 
 const router = Router();
 
-router.post('/login');
-router.get('/users');
+router.post('/login', userController.login);
+router.get('/users', userController.listUser);
 
 module.exports = router
