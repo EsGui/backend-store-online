0eb4fd5a7e3474f4da3d73ef48e07db89bd167fc
EsGui <guilhermedagama2021100@gmail.com>
2022-10-17 01:29:53 -0300

    feat: Foi incluido todas as associações necessárias no objeto que contém as informações do usuário

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -8,7 +8,14 @@ const authController = {
       return res.status(400).json({ message: "Token not found!" })
     }
     const dataToken = authService.authToken(authorization);
-    const userLogged = await User.findOne({ where: { email: dataToken.data.email } });
+    const userLogged = await User.findOne({ where: { email: dataToken.data.email }, include: [
+      { model: Product, as: 'product' }, 
+      { model: Favorites, as: 'productFavorite' },
+      { model: Cart, as: 'cartProductsUserId' },
+      /* { model: Cart, as: 'cartProductsSellerId' }, */
+      { model: Sales, as: 'salesProductsSeller' },
+      { model: Sales, as: 'salesProductsBuyer' }
+    ] });
     return res.status(200).json({ ...userLogged.dataValues, status: 200, token: authorization });
   },
 };
